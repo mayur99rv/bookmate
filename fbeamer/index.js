5014754405e24130853e308629c7236d2e5c9514
mayur99rv <66148617+mayur99rv@users.noreply.github.com>
2021-10-11 13:30:36 +0530

    Update index.js

--- fbeamer/index.js
+++ fbeamer/index.js
@@ -21,6 +21,7 @@ class FBeamer {
 		// If req.query.hub.mode is 'subscribe'
 		// and if req.query.hub.verify_token is the same as this.VERIFY_TOKEN
 		// then send back an HTTP status 200 and req.query.hub.challenge
+		console.log(req.query);
 		let {mode, verify_token, challenge} = req.query.hub;
 
 		if(mode === 'subscribe' && verify_token === this.VERIFY_TOKEN) {
